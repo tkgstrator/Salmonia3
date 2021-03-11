a8e7d33cd351730e9c8918a1e5f3018fd9b743f4
tkgstrator <nasawake.am@gmail.com>
2021-03-11 21:51:57 +0900

    Initial Commit

---
+++ Salmonia3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Salmonia3
+//
+//  Created by Devonly on 2021/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
