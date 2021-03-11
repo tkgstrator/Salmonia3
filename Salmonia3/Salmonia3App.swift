261a89d0a0c64fa2540ab3e65a8fdd06125b040e
tkgstrator <tkgstrator@users.noreply.github.com>
2021-03-11 21:51:57 +0900

    Initial Commit

---
+++ Salmonia3/Salmonia3App.swift
@@ -0,0 +1,17 @@
+//
+//  Salmonia3App.swift
+//  Salmonia3
+//
+//  Created by Devonly on 2021/03/11.
+//
+
+import SwiftUI
+
+@main
+struct Salmonia3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
