977afd41c1b916beb2cf46e6402b63f06e8eba53
tkgstrator <nasawake.am@gmail.com>
2021-03-13 12:19:43 +0900

    - FirebaseUI/OAuthを追加

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '14.0'
+use_frameworks!
+
+target "Salmonia3" do
+pod 'FirebaseUI/Auth'
+pod 'FirebaseUI/OAuth'
+end
---
+++ Pods/Target Support Files/Pods-Salmonia3/Pods-Salmonia3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Salmonia3 : NSObject
+@end
+@implementation PodsDummy_Pods_Salmonia3
+@end
