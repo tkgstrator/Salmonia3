77de928600afaa76c893459e0d38ab5b60c6b7bd
tkgstrator <tkgstrator@users.noreply.github.com>
2021-03-14 13:07:36 +0900

    - RealmPlayerResultの一部をCodableに対応

--- Salmonia3/Assets/Realm/RealmPlayerResult.swift
+++ Salmonia3/Assets/Realm/RealmPlayerResult.swift
@@ -25,18 +25,46 @@ class RealmPlayerResult: Object, Decodable {
         return ["nsaid"]
     }
     
-    private enum Codingkeys: String, CodingKey {
+    private enum CodingKeys: String, CodingKey {
         case name           = "name"
         case pid            = "pid"
         case deadCount      = "dead_count"
         case helpCount      = "help_count"
         case goldenIkuraNum = "golden_ikura_num"
         case ikuraNum       = "ikura_num"
+        case specialId      = "special"
         case weaponList     = "weapon_list"
-        case specialCount   = "special_count"
+        case specialCount   = "special_counts"
+        case bossKillCounts = "boss_kill_counts"
     }
     
-    public required convenience init(from decoder: Decoder) {
+    public required convenience init(from decoder: Decoder) throws {
         self.init()
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        name = try? container.decode(String.self, forKey: .name)
+        pid = try? container.decode(String.self, forKey: .pid)
+        deadCount = try container.decode(Int.self, forKey: .deadCount)
+        helpCount = try container.decode(Int.self, forKey: .helpCount)
+        goldenIkuraNum = try container.decode(Int.self, forKey: .goldenIkuraNum)
+        ikuraNum = try container.decode(Int.self, forKey: .ikuraNum)
+//        let _weaponList = try container.decodeIfPresent(Int.self, forKey: .weaponList)
+//        print(_weaponList)
+//        let _bossKillCounts = try container.decodeIfPresent([BossCounts].self, forKey: .bossKillCounts) ?? [BossCounts()]
+        let _weaponList = try container.decodeIfPresent([WeaponList].self, forKey: .weaponList) ?? [WeaponList()]
+        print(_weaponList)
+        weaponList.append(objectsIn: _weaponList.map{ Int($0.id).intValue })
+        let _specialCount = try container.decodeIfPresent([Int].self, forKey: .specialCount) ?? [Int()]
+        specialCount.append(objectsIn: _specialCount)
     }
+}
+
+class WeaponList: Codable {
+    var id: String = "0"
+    var weapon: [String: String] = [:]
+}
+
+class BossCounts: Codable {
+    var id: String = "0"
+    var count: Int = 0
 }
