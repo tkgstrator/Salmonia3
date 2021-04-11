f7ae11cc39fce1bff8d85f6e396ccb5f91aa890e
tkgstrator <nasawake.am@gmail.com>
2021-04-11 12:44:46 +0900

    - Observerクラスを作成

--- Salmonia3/ObservableObject/CoreRealmCoop.swift
+++ Salmonia3/ObservableObject/CoreRealmCoop.swift
@@ -11,20 +11,17 @@ import Combine
 import SwiftUI
 
 class CoreRealmCoop: ObservableObject {
-    var objectWillChange: ObservableObjectPublisher = .init()
     private static var realm = try! Realm()
-    private var token: NotificationToken?
-    private var futureRotation: NSKeyValueObservation?
+    private var token: NSObserver = NSObserver()
 
     // 実際に使いそうなデータ
     @Published var results: RealmSwift.Results<RealmCoopResult> = realm.objects(RealmCoopResult.self).sorted(byKeyPath: "playTime", ascending: false)
     @Published var shifts: RealmSwift.Results<RealmCoopShift> = realm.objects(RealmCoopShift.self).sorted(byKeyPath: "startTime", ascending: false)
-
     @AppStorage("FEATURE_FREE_02") var isFree02: Bool = false
 
     private func rotationObserver() {
         // 将来のシフト表示が切り替わったとき
-        futureRotation = UserDefaults.standard.observe(\.FEATURE_FREE_02, options: [.initial, .new], changeHandler: { [self] (_, _) in
+        token.futureRotation = UserDefaults.standard.observe(\.FEATURE_FREE_02, options: [.initial, .new], changeHandler: { [self] (_, _) in
             switch isFree02 {
             case true:
                 // 全部表示
@@ -45,14 +42,24 @@ class CoreRealmCoop: ObservableObject {
     }
 
     init() {
-        token = try? Realm().objects(RealmCoopResult.self).observe { [self] _ in
-            #warning("何故か一回しか呼ばれない")
+        token.realm = try? Realm().objects(RealmCoopResult.self).observe { [self] _ in
+            rotationObserver()
+            results = CoreRealmCoop.realm.objects(RealmCoopResult.self).sorted(byKeyPath: "playTime", ascending: false)
+        }
+        
+        token.realm = try? Realm().objects(RealmUserInfo.self).observe { [self] _ in
             rotationObserver()
             results = CoreRealmCoop.realm.objects(RealmCoopResult.self).sorted(byKeyPath: "playTime", ascending: false)
         }
     }
 
     deinit {
-        futureRotation?.invalidate()
+        token.futureRotation?.invalidate()
     }
+}
+
+fileprivate struct NSObserver {
+    var realm: NotificationToken?
+    var rareWeapon: NSKeyValueObservation?
+    var futureRotation: NSKeyValueObservation?
 }
