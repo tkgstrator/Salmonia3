10b0fcb6b623aacf7eab9a487a46bb8bb1e79adc
tkgstrator <nasawake.am@gmail.com>
2021-05-04 16:24:14 +0900

    - 取り込み機能を修正

--- Salmonia3/Extension/String.swift
+++ Salmonia3/Extension/String.swift
@@ -12,4 +12,17 @@ extension String {
     var localized: String {
         return NSLocalizedString(self, comment: "")
     }
+    
+    func capture(pattern: String, group: Int) -> String? {
+        let result = capture(pattern: pattern, group: [group])
+        return result.isEmpty ? nil : result[0]
+    }
+
+    private func capture(pattern: String, group: [Int]) -> [String] {
+        guard let regex = try? NSRegularExpression(pattern: pattern) else { return [] }
+        guard let matched = regex.firstMatch(in: self, range: NSRange(location: 0, length: self.count)) else { return [] }
+        return group.map { group -> String in
+            return (self as NSString).substring(with: matched.range(at: group))
+        }
+    }
 }
--- Salmonia3/MenuView/Loading/ImportingView.swift
+++ Salmonia3/MenuView/Loading/ImportingView.swift
@@ -8,44 +8,49 @@
 import SwiftUI
 import Combine
 import SalmonStats
+import SplatNet2
 
 struct ImportingView: View {
     @Environment(\.presentationMode) var present
     @State var data: ProgressData = ProgressData()
     @State var task = Set<AnyCancellable>()
-    
-    private func dismiss() {
-        DispatchQueue.main.async { present.wrappedValue.dismiss() }
-    }
-    
+
     var body: some View {
         LoggingThread(data: $data)
             .onAppear {
-                let nsaid = "91d160aa84e88da6"
-                SalmonStats.shared.getMetadata(nsaid: nsaid)
-                    .sink(receiveCompletion: { completion in
-                    }, receiveValue: { metadata in
-                        for userdata in metadata {
-                            let lastPageId: Int = Int((userdata.results.clear + userdata.results.fail) / 200) + 1
-                            for pageId in Range(1 ... 1) {
-                                SalmonStats.shared.getResults(nsaid: userdata.playerId, pageId: pageId)
-                                    .sink(receiveCompletion: { completion in
-                                        switch completion {
-                                        case .finished:
-                                            print("FINISHED")
-                                        case .failure(let error):
-                                            print(error)
-                                        }
-                                    }, receiveValue: { results in
-                                        RealmManager.addNewResultsFromSalmonStats(from: results, pid: nsaid)
-                                    })
-                                    .store(in: &task)
-                            }
-                        }
-                    })
-                    .store(in: &task)
+                if let nsaid = SplatNet2.shared.playerId {
+                    importResultFromSalmonStats(nsaid: nsaid)
+                    present.wrappedValue.dismiss()
+                } else {
+                    present.wrappedValue.dismiss()
+                }
             }
             .navigationTitle("TITLE_IMPORT_SALMONSTATS")
+    }
+    
+    private func importResultFromSalmonStats(nsaid: String) {
+        SalmonStats.shared.getMetadata(nsaid: nsaid)
+            .sink(receiveCompletion: { completion in
+            }, receiveValue: { metadata in
+                for userdata in metadata {
+                    let lastPageId: Int = Int((userdata.results.clear + userdata.results.fail) / 200) + 1
+                    for pageId in Range(1 ... 1) {
+                        SalmonStats.shared.getResults(nsaid: userdata.playerId, pageId: pageId)
+                            .sink(receiveCompletion: { completion in
+                                switch completion {
+                                case .finished:
+                                    print("FINISHED")
+                                case .failure(let error):
+                                    print(error)
+                                }
+                            }, receiveValue: { results in
+                                RealmManager.addNewResultsFromSalmonStats(from: results, pid: nsaid)
+                            })
+                            .store(in: &task)
+                    }
+                }
+            })
+            .store(in: &task)
     }
 }
 
