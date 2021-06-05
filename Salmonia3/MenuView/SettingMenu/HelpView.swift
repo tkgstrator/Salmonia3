39f24c543521b2c34c687e686245d802be533747
tkgstrator <tkgstrator@users.noreply.github.com>
2021-06-06 01:53:33 +0900

    - バージョン更新

--- Salmonia3/MenuView/SettingMenu/HelpView.swift
+++ Salmonia3/MenuView/SettingMenu/HelpView.swift
@@ -12,7 +12,7 @@ struct HelpView: View {
     @State var url: URL?
     
     var body: some View {
-        Form {
+        List {
             Button(action: { url = URL(string: "https://github.com/tkgstrator/Salmonia3/raw/develop/Resources/06.png") }, label: { Text(.HELP_GET_RESULT).font(.custom("Splatfont2", size: 16)) })
             Button(action: { url = URL(string: "https://github.com/tkgstrator/Salmonia3/raw/develop/Resources/07.png") }, label: { Text(.HELP_IMPORT_RESULT).font(.custom("Splatfont2", size: 16)) })
         }
