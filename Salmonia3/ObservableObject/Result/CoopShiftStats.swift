8b97bd79fc39c011676f4a6d7df0b993099f310b
tkgstrator <tkgstrator@users.noreply.github.com>
2021-06-12 13:55:21 +0900

    - スペシャル支給率をグラフ表示

--- Salmonia3/ObservableObject/Result/CoopShiftStats.swift
+++ Salmonia3/ObservableObject/Result/CoopShiftStats.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import RealmSwift
+import SwiftUI
+import SwiftChart
 
 final class CoopShiftStats: ObservableObject {
     @Published var resultAvg: ResultAvg?
@@ -40,8 +42,9 @@ final class CoopShiftStats: ObservableObject {
         token?.invalidate()
     }
     
+    // MARK: 指定されたスケジュールのブキのリストを返す
     private func getWeaponData(startTime: Int, nsaid: [String]) -> [ResultWeapon] {
-        let shift = RealmManager.shared.realm.objects(RealmCoopShift.self).filter("startTime=%@", startTime).first!
+        guard let shift = RealmManager.shared.realm.objects(RealmCoopShift.self).filter("startTime=%@", startTime).first else { return [] }
         let suppliedWepons: [Int] = RealmManager.shared.realm.objects(RealmPlayerResult.self).filter("ANY result.startTime=%@ AND pid IN %@", startTime, nsaid).flatMap{ $0.weaponList }
         let allWeaponLists: [Int] = Array(WeaponType.allCases.map{ $0.rawValue })
 
@@ -66,36 +69,47 @@ final class CoopShiftStats: ObservableObject {
         }
     }
 
-    class ResultOverView {
+    // MARK: シフト統計の大雑把な値を返す
+    final class ResultOverView {
         var jobNum: Int?
         var clearWave: Double?
         var clearRatio: Double?
         var goldenEggRatio: Double?
         var powerEggRatio: Double?
-        var specialWeapon: [Double?] = []
+        var specialWeapon: PieChartData?
+        var playerBossDefeatedRatio: [Double?] = []
+        var otherBossDefeatedRatio: [Double?] = []
+        var teamBossDefeatedRatio: [Double?] = []
+        var bossAppearRatio: [Double?] = []
         
         init(results: RealmSwift.Results<RealmCoopResult>, player: RealmSwift.Results<RealmPlayerResult>) {
             guard let _ = results.first else { return }
-            jobNum = results.count
-            clearRatio = calcRatio(results.filter("isClear=true").count, divideBy: jobNum)
-            goldenEggRatio = calcRatio(player.sum(ofProperty: "goldenIkuraNum"), divideBy: results.sum(ofProperty: "goldenEggs"))
-            powerEggRatio = calcRatio(player.sum(ofProperty: "ikuraNum"), divideBy: results.sum(ofProperty: "powerEggs"))
-            specialWeapon = [
-                calcRatio(player.filter("specialId=%@", 2).count, divideBy: jobNum),
-                calcRatio(player.filter("specialId=%@", 7).count, divideBy: jobNum),
-                calcRatio(player.filter("specialId=%@", 8).count, divideBy: jobNum),
-                calcRatio(player.filter("specialId=%@", 9).count, divideBy: jobNum)
-            ]
+            self.jobNum = results.count
+            self.clearRatio = calcRatio(results.filter("isClear=true").count, divideBy: jobNum)
+            self.goldenEggRatio = calcRatio(player.sum(ofProperty: "goldenIkuraNum"), divideBy: results.sum(ofProperty: "goldenEggs"))
+            self.powerEggRatio = calcRatio(player.sum(ofProperty: "ikuraNum"), divideBy: results.sum(ofProperty: "powerEggs"))
+            self.specialWeapon = PieChartData(data: SpecialType.allCases.map({ DataSet(value: Double(player.filter("specialId=%@", $0.rawValue).count) / Double(jobNum ?? 1), caption: $0.name)}))
+
+            // MARK: 各オオモノの出現数を保存
+            let bossAppearCount = Array(results.map({ $0.bossCounts })).sum()
+            let bossKillCount = Array(results.map({ $0.bossKillCounts })).sum()
+            let playerBossKillCount = Array(player.map({ $0.bossKillCounts })).sum()
+            let otherBossKillCount = Array(zip(bossAppearCount, playerBossKillCount).map({ $0.0 - $0.1 }))
+            
+            self.playerBossDefeatedRatio = bossDefeatedRatio(playerBossKillCount, bossAppearCount)
+            self.teamBossDefeatedRatio = bossDefeatedRatio(bossKillCount, bossAppearCount)
+            self.otherBossDefeatedRatio = bossDefeatedRatio(otherBossKillCount, bossKillCount.map({ $0 * 3}))
+            let total: Int = bossAppearCount.drop(index: [0, 7]).reduce(0, +)
+            self.bossAppearRatio = bossAppearCount.drop(index: [0, 7]).map({ Double($0) / Double(total) })
         }
     }
-    
-    class ResultWeapon: Identifiable {
+
+    // MARK: 出現したブキのカウントを行う
+    final class ResultWeapon: Identifiable {
         var id: String = UUID().uuidString
         var weaponId: Int
         var count: Int = 0
-        var image: String {
-            return String(weaponId).imageURL
-        }
+        var image: String { String(weaponId).imageURL }
         
         init(weaponId: Int, count: Int) {
             self.weaponId = weaponId
@@ -103,7 +117,8 @@ final class CoopShiftStats: ObservableObject {
         }
     }
     
-    class ResultMax {
+    // MARK: 最高の評価を取得
+    final class ResultMax {
         var teamPowerEggs: Int?
         var teamGoldenEggs: Int?
         var powerEggs: Int?
@@ -123,7 +138,8 @@ final class CoopShiftStats: ObservableObject {
         }
     }
 
-    class ResultAvg {
+    // MARK: 平均の評価を取得
+    final class ResultAvg {
         var teamPowerEggs: Double?
         var teamGoldenEggs: Double?
         var powerEggs: Double?
@@ -166,3 +182,30 @@ fileprivate extension Array where Element == Int {
     }
 }
 
+extension Collection where Element == RealmSwift.List<Int> {
+    func sum() -> [Int] {
+        guard let element = self.first else { return [] }
+        
+        var value: [Int] = Array(repeating: 0, count: element.count)
+        for array in self {
+            value = Array(zip(Array(array), value).map({ $0.0 + $0.1 }))
+        }
+        return value
+    }
+}
+
+private func bossDefeatedRatio<T: Collection, U: FloatingPoint>(_ a: T, _ b: T) -> T where T.Element == U {
+    if a.count != b.count { fatalError() }
+    return Array(zip(a, b).map({ $0.0 / $0.1 })) as! T
+}
+
+private func bossDefeatedRatio(_ a: [Int], _ b: [Int] ) -> [Double] {
+    if a.count != b.count { fatalError() }
+    return Array(zip(a, b).map({ Double($0.0) / Double($0.1) }))
+}
+
+private extension Collection where Element == Int {
+    func drop(index: [Int]) -> [Int] {
+        return self.filter({ !index.contains(self.firstIndex(of: $0) as! Int) })
+    }
+}
