35aec5e99cfd90fa41a3dccc79b697657c0945f9
tkgstrator <nasawake.am@gmail.com>
2021-07-01 11:52:22 +0900

    - WAVE分析の仮表示

--- Salmonia3/MenuView/Collection/Shift/StatsTab/StatsWaveView.swift
+++ Salmonia3/MenuView/Collection/Shift/StatsTab/StatsWaveView.swift
@@ -13,10 +13,12 @@ struct StatsWaveView: View {
     
     var body: some View {
         ScrollView {
-//            LazyVGrid(columns: Array(repeating: .init(.flexible()), count: 3, alignment: .center, spacing: nil, pinnedViews: [])) {
-//                Text("")
-//            }
-            Text("AAA")
+            LazyVGrid(columns: Array(repeating: .init(.flexible(maximum: 200)), count: 3), alignment: .center, spacing: nil, pinnedViews: []) {
+                ForEach(stats.resultWave.indices) { index in
+                    CircleWaveView(tide: 80)
+                        .overlay(Text(stats.resultWave[index].goldenEggs.stringValue))
+                }
+            }
         }
     }
 }
