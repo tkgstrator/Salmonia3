72a5c3e03022459d23c26f091b44f5f76174ab31
tkgstrator <tkgstrator@users.noreply.github.com>
2021-07-09 16:10:47 +0900

    - RealmCoopShiftの更新

--- Salmonia3/Realm/RealmCoopShift.swift
+++ Salmonia3/Realm/RealmCoopShift.swift
@@ -10,23 +10,20 @@ import RealmSwift
 import SplatNet2
 
 final class RealmCoopShift: Object, Identifiable {
-    @objc dynamic var startTime: Int = 0
-    @objc dynamic var endTime: Int = 0
-    let stageId = RealmOptional<Int>()
-    let rareWeapon = RealmOptional<Int>()
-    dynamic var weaponList = List<Int>()
-
-    override static func primaryKey() -> String? {
-        return "startTime"
-    }
+    
+    @Persisted(primaryKey: true) var startTime: Int
+    @Persisted var endTime: Int
+    @Persisted var stageId: Int?
+    @Persisted var rareWeapon: Int?
+    @Persisted var weaponList: List<Int>
 
     convenience init(from response: Response.ScheduleCoop) {
         self.init()
         self.startTime = response.startTime
         self.endTime = response.endTime
-        self.stageId.value = response.stageId
+        self.stageId = response.stageId
         if response.weaponList.contains(-1) {
-            self.rareWeapon.value = response.rareWeapon
+            self.rareWeapon = response.rareWeapon
         }
         self.weaponList.append(objectsIn: response.weaponList)
     }
