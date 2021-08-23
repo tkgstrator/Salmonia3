66c2e4bbb44e580f47adf4f5809bba2482ece4e9
tkgstrator <tkgstrator@users.noreply.github.com>
2021-08-24 00:07:32 +0900

    - 表示スタイルの追加

--- Salmonia3/Enum/Appearance.swift
+++ Salmonia3/Enum/Appearance.swift
@@ -6,11 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
 
-enum ListStyle: String, CaseIterable, Identifiable {
+/// リザルト表示のリスト形式
+enum ResultListStyle: String, CaseIterable, Identifiable {
     var id: String { rawValue }
     case legacy     = "STYLE_LEGACY"
-    case `default`     = "STYLE_DEFAULT"
+    case plain      = "STYLE_PLAIN"
+    case grouped    = "STYLE_GROUPED"
+    case `default`  = "STYLE_DEFAULT"
     case sidebar    = "STYLE_SIDEBAR"
     case inset      = "STYLE_INSET"
 }
+
+//extension ResultListStyle {
+//    var listStyle: SwiftUI.ListStyle {
+//        switch self {
+//        case .grouped:
+//            return GroupedListStyle()
+//        case .default:
+//            return DefaultListStyle
+//        case .sidebar:
+//            return SidebarListStyle
+//        case .inset:
+//            return InsetListStyle
+//        default:
+//            return PlainListStyle
+//        }
+//    }
+//}
